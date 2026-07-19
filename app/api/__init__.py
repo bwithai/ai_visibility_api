5c351335873b149668c7260b2d406f309fccdb94
bwithai <sanaullah41821@gmail.com>
2026-07-19 10:29:52 +0500

    profile router declare for create/get

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify
 
+from app.api.profiles import profiles_bp
+
 api_bp = Blueprint("api", __name__)
+api_bp.register_blueprint(profiles_bp)
 
 
 @api_bp.get("/hello")
